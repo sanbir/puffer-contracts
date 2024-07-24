7f9b04e1ded10c528fe241967b9d884fe1ee8cc9
Alexander Biryukov <alexander.biryukov@p2p.org>
2024-07-24 13:10:33 +0400

    safe on mainnet works

--- utility-scripts/script/GenerateBLSKeysAndRegisterValidatorsCalldata.s.sol
+++ utility-scripts/script/GenerateBLSKeysAndRegisterValidatorsCalldata.s.sol
@@ -5,7 +5,6 @@ import "forge-std/Script.sol";
 import "forge-std/console2.sol";
 import { stdJson } from "forge-std/StdJson.sol";
 import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
-import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "../src/ISSVClusters.sol";
 import "../src/IP2pSsvProxyFactory.sol";
 
@@ -20,24 +19,12 @@ import "../src/IP2pSsvProxyFactory.sol";
 contract GenerateBLSKeysAndRegisterValidatorsCalldata is Script {
     using stdJson for string;
 
-    address public ssvNtworkAddress = 0x38A4794cCEd47d3baf7370CcC43B560D3a1beEFA;
-    address public safe = 0x9aB843F2d60be2316F42B9764e98b532908AaB37;
-
-    address public p2pSsvProxyFactoryAddress = 0x87A11cF5aFF0a46A7Ffb4C00b497aa3a73015d51;
+    address public ssvNtworkAddress = 0xDD9BC35aE942eF0cFa76930954a156B3fF30a4E1;
+    address public safe = 0xe22dE1d7A27AfaB02fC25F9b2B0cb538a26548c2;
 
     struct Tx {
         address to;
         bytes data;
-    }
-
-    struct JsonEntry {
-        address owner;
-        uint256[] operator_ids;
-        bytes[] pubkeys;
-    }
-
-    struct JsonFull {
-        JsonEntry[] arr;
     }
 
     function run() public {
@@ -50,17 +37,45 @@ contract GenerateBLSKeysAndRegisterValidatorsCalldata is Script {
             transactions[i] = getTx(i, json);
         }
 
-//        Tx[] memory transactions = new Tx[](4);
-//        transactions[0] = getTx1();
-//        transactions[1] = getTx2();
-//        transactions[2] = getTx3();
-//        transactions[3] = getTx4();
-//
-////        Tx[] memory transactions = new Tx[](2);
-////        transactions[0] = getTx__TEST1();
-////        transactions[1] = getTx__TEST2();
-//
         _createSafeJson(safe, transactions);
+    }
+
+    function getCluster(uint256 i) internal returns(ISSVClusters.Cluster memory cluster) {
+        if (i == 0) {
+            cluster = ISSVClusters.Cluster({
+                validatorCount: 500,
+                networkFeeIndex: 75868432269,
+                index: 46873400,
+                active: true,
+                balance: 109389392792800000000
+            });
+        } else if (i == 1) {
+            cluster = ISSVClusters.Cluster({
+                validatorCount: 250,
+                networkFeeIndex: 75996720789,
+                index: 0,
+                active: true,
+                balance: 97235863316000000000
+            });
+        } else if (i == 2) {
+            cluster = ISSVClusters.Cluster({
+                validatorCount: 500,
+                networkFeeIndex: 75881049729,
+                index: 0,
+                active: true,
+                balance: 106821837126500000000
+            });
+        } else if (i == 3) {
+            cluster = ISSVClusters.Cluster({
+                validatorCount: 500,
+                networkFeeIndex: 75988446774,
+                index: 0,
+                active: true,
+                balance: 116417868572000000000
+            });
+        } else {
+            revert("No cluster");
+        }
     }
 
     function getTx(uint256 i, string memory json) internal returns(Tx memory tx) {
@@ -75,13 +90,7 @@ contract GenerateBLSKeysAndRegisterValidatorsCalldata is Script {
         operatorIds[2] = uint64(operator_ids[2]);
         operatorIds[3] = uint64(operator_ids[3]);
 
-        ISSVClusters.Cluster memory cluster = ISSVClusters.Cluster({
-            validatorCount: 5,
-            networkFeeIndex: 61300535088,
-            index: 262565454206,
-            active: true,
-            balance: 53460049500000000000
-        });
+        ISSVClusters.Cluster memory cluster = getCluster(i);
 
         bytes memory bulkRemoveValidatorCalldata = abi.encodeCall(
             ISSVClusters.bulkRemoveValidator,
@@ -91,156 +100,6 @@ contract GenerateBLSKeysAndRegisterValidatorsCalldata is Script {
         tx = Tx({ to: ssvNtworkAddress, data: bulkRemoveValidatorCalldata });
     }
 
-    function getTx1() internal returns(Tx memory tx) {
-        bytes[] memory publicKeys = new bytes[](7);
-        publicKeys[0] = bytes(hex'8ae430787d3cb55093dba32d4f5e7736c48d494121dc6ed5520182490b977af7ddc0ecc586296fc315f9553dd07c5486');
-        publicKeys[1] = bytes(hex'b87798a82ca43743b7b726bdad694c8c9b76b68023bde42cf97a0e4d0da7e3604d4b436b9db44ef5e56106626d95ef68');
-        publicKeys[2] = bytes(hex'a3e789d3b378c42a3c963ef549b739ca55d1cdc7211aa22ebf382e4e847ea23f12c5a5131aa835afe5743432b02df806');
-        publicKeys[3] = bytes(hex'8171e4c04ea01ab1a84746527e33fdc739ad118946ba3f1cee356f3b35e9642304a7efffd1e4db4aa556ce333687ea11');
-        publicKeys[4] = bytes(hex'95dc732567172754d9a1764b0ce4aefba1c25d82983d964bafd26e8776c8b8197695b569b00321fa581eb70d528bca57');
-        publicKeys[5] = bytes(hex'92269be7b88c417f46aafd18943783990df0952c9858bd3147815e31f48d1be5f559387b696231f98fcc5ca2b3ff354e');
-        publicKeys[6] = bytes(hex'a413b93c25f77d27c5edab2b131d06bc90215c3eb2761309fdac35b0c76e12dccf45788a7ca0228b591cafb8626ab01a');
-
-        uint64[] memory operatorIds = new uint64[](4);
-        operatorIds[0] = 42;
-        operatorIds[0] = 43;
-        operatorIds[0] = 44;
-        operatorIds[0] = 45;
-
-        ISSVClusters.Cluster memory cluster = ISSVClusters.Cluster({
-            validatorCount: 5,
-            networkFeeIndex: 61300535088,
-            index: 262565454206,
-            active: true,
-            balance: 53460049500000000000
-        });
-
-        bytes memory bulkRemoveValidatorCalldata = abi.encodeCall(
-            ISSVClusters.bulkRemoveValidator,
-            (publicKeys, operatorIds, cluster)
-        );
-
-        tx = Tx({ to: ssvNtworkAddress, data: bulkRemoveValidatorCalldata });
-    }
-
-    function getTx2() internal returns(Tx memory tx) {
-        bytes[] memory publicKeys = new bytes[](7);
-        publicKeys[0] = bytes(hex'8ae430787d3cb55093dba32d4f5e7736c48d494121dc6ed5520182490b977af7ddc0ecc586296fc315f9553dd07c5486');
-        publicKeys[1] = bytes(hex'b87798a82ca43743b7b726bdad694c8c9b76b68023bde42cf97a0e4d0da7e3604d4b436b9db44ef5e56106626d95ef68');
-        publicKeys[2] = bytes(hex'a3e789d3b378c42a3c963ef549b739ca55d1cdc7211aa22ebf382e4e847ea23f12c5a5131aa835afe5743432b02df806');
-        publicKeys[3] = bytes(hex'8171e4c04ea01ab1a84746527e33fdc739ad118946ba3f1cee356f3b35e9642304a7efffd1e4db4aa556ce333687ea11');
-        publicKeys[4] = bytes(hex'95dc732567172754d9a1764b0ce4aefba1c25d82983d964bafd26e8776c8b8197695b569b00321fa581eb70d528bca57');
-        publicKeys[5] = bytes(hex'92269be7b88c417f46aafd18943783990df0952c9858bd3147815e31f48d1be5f559387b696231f98fcc5ca2b3ff354e');
-        publicKeys[6] = bytes(hex'a413b93c25f77d27c5edab2b131d06bc90215c3eb2761309fdac35b0c76e12dccf45788a7ca0228b591cafb8626ab01a');
-
-        uint64[] memory operatorIds = new uint64[](4);
-        operatorIds[0] = 42;
-        operatorIds[0] = 43;
-        operatorIds[0] = 44;
-        operatorIds[0] = 45;
-
-        ISSVClusters.Cluster memory cluster = ISSVClusters.Cluster({
-            validatorCount: 5,
-            networkFeeIndex: 61300535088,
-            index: 262565454206,
-            active: true,
-            balance: 53460049500000000000
-        });
-
-        bytes memory bulkRemoveValidatorCalldata = abi.encodeCall(
-            ISSVClusters.bulkRemoveValidator,
-            (publicKeys, operatorIds, cluster)
-        );
-
-        tx = Tx({ to: ssvNtworkAddress, data: bulkRemoveValidatorCalldata });
-    }
-
-    function getTx3() internal returns(Tx memory tx) {
-        bytes[] memory publicKeys = new bytes[](7);
-        publicKeys[0] = bytes(hex'8ae430787d3cb55093dba32d4f5e7736c48d494121dc6ed5520182490b977af7ddc0ecc586296fc315f9553dd07c5486');
-        publicKeys[1] = bytes(hex'b87798a82ca43743b7b726bdad694c8c9b76b68023bde42cf97a0e4d0da7e3604d4b436b9db44ef5e56106626d95ef68');
-        publicKeys[2] = bytes(hex'a3e789d3b378c42a3c963ef549b739ca55d1cdc7211aa22ebf382e4e847ea23f12c5a5131aa835afe5743432b02df806');
-        publicKeys[3] = bytes(hex'8171e4c04ea01ab1a84746527e33fdc739ad118946ba3f1cee356f3b35e9642304a7efffd1e4db4aa556ce333687ea11');
-        publicKeys[4] = bytes(hex'95dc732567172754d9a1764b0ce4aefba1c25d82983d964bafd26e8776c8b8197695b569b00321fa581eb70d528bca57');
-        publicKeys[5] = bytes(hex'92269be7b88c417f46aafd18943783990df0952c9858bd3147815e31f48d1be5f559387b696231f98fcc5ca2b3ff354e');
-        publicKeys[6] = bytes(hex'a413b93c25f77d27c5edab2b131d06bc90215c3eb2761309fdac35b0c76e12dccf45788a7ca0228b591cafb8626ab01a');
-
-        uint64[] memory operatorIds = new uint64[](4);
-        operatorIds[0] = 42;
-        operatorIds[0] = 43;
-        operatorIds[0] = 44;
-        operatorIds[0] = 45;
-
-        ISSVClusters.Cluster memory cluster = ISSVClusters.Cluster({
-            validatorCount: 5,
-            networkFeeIndex: 61300535088,
-            index: 262565454206,
-            active: true,
-            balance: 53460049500000000000
-        });
-
-        bytes memory bulkRemoveValidatorCalldata = abi.encodeCall(
-            ISSVClusters.bulkRemoveValidator,
-            (publicKeys, operatorIds, cluster)
-        );
-
-        tx = Tx({ to: ssvNtworkAddress, data: bulkRemoveValidatorCalldata });
-    }
-
-    function getTx4() internal returns(Tx memory tx) {
-        bytes[] memory publicKeys = new bytes[](7);
-        publicKeys[0] = bytes(hex'8ae430787d3cb55093dba32d4f5e7736c48d494121dc6ed5520182490b977af7ddc0ecc586296fc315f9553dd07c5486');
-        publicKeys[1] = bytes(hex'b87798a82ca43743b7b726bdad694c8c9b76b68023bde42cf97a0e4d0da7e3604d4b436b9db44ef5e56106626d95ef68');
-        publicKeys[2] = bytes(hex'a3e789d3b378c42a3c963ef549b739ca55d1cdc7211aa22ebf382e4e847ea23f12c5a5131aa835afe5743432b02df806');
-        publicKeys[3] = bytes(hex'8171e4c04ea01ab1a84746527e33fdc739ad118946ba3f1cee356f3b35e9642304a7efffd1e4db4aa556ce333687ea11');
-        publicKeys[4] = bytes(hex'95dc732567172754d9a1764b0ce4aefba1c25d82983d964bafd26e8776c8b8197695b569b00321fa581eb70d528bca57');
-        publicKeys[5] = bytes(hex'92269be7b88c417f46aafd18943783990df0952c9858bd3147815e31f48d1be5f559387b696231f98fcc5ca2b3ff354e');
-        publicKeys[6] = bytes(hex'a413b93c25f77d27c5edab2b131d06bc90215c3eb2761309fdac35b0c76e12dccf45788a7ca0228b591cafb8626ab01a');
-
-        uint64[] memory operatorIds = new uint64[](4);
-        operatorIds[0] = 42;
-        operatorIds[0] = 43;
-        operatorIds[0] = 44;
-        operatorIds[0] = 45;
-
-        ISSVClusters.Cluster memory cluster = ISSVClusters.Cluster({
-            validatorCount: 5,
-            networkFeeIndex: 61300535088,
-            index: 262565454206,
-            active: true,
-            balance: 53460049500000000000
-        });
-
-        bytes memory bulkRemoveValidatorCalldata = abi.encodeCall(
-            ISSVClusters.bulkRemoveValidator,
-            (publicKeys, operatorIds, cluster)
-        );
-
-        tx = Tx({ to: ssvNtworkAddress, data: bulkRemoveValidatorCalldata });
-    }
-
-    function getTx__TEST1() internal returns(Tx memory tx) {
-        bytes memory setMaxSsvTokenAmountPerValidatorCalldata = abi.encodeCall(
-            IP2pSsvProxyFactory.setMaxSsvTokenAmountPerValidator,
-            (50 ether)
-        );
-
-        tx = Tx({ to: p2pSsvProxyFactoryAddress, data: setMaxSsvTokenAmountPerValidatorCalldata });
-    }
-
-    function getTx__TEST2() internal returns(Tx memory tx) {
-        bytes4[] memory selectors = new bytes4[](2);
-        selectors[0] = 0x6a761203;
-        selectors[1] = 0x6a761204;
-
-        bytes memory setAllowedSelectorsForOperatorCalldata = abi.encodeCall(
-            IP2pSsvProxyFactory.setAllowedSelectorsForOperator,
-            (selectors)
-        );
-
-        tx = Tx({ to: p2pSsvProxyFactoryAddress, data: setAllowedSelectorsForOperatorCalldata });
-    }
-
     function _createSafeJson(address safe, Tx[] memory transactions) internal {
         // First we need to craft the JSON file for the transactions batch
         string memory root = "root";
@@ -248,7 +107,7 @@ contract GenerateBLSKeysAndRegisterValidatorsCalldata is Script {
         vm.serializeString(root, "version", "1.0");
         vm.serializeUint(root, "createdAt", block.timestamp * 1000);
         // Needs to be a string
-        vm.serializeString(root, "chainId", Strings.toString(block.chainid));
+        vm.serializeString(root, "chainId", "1");
 
         string memory meta = "meta";
         vm.serializeString(meta, "name", "Transactions Batch");
